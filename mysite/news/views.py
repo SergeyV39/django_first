0b33a0c913276b228f5dc4d1f93226898e5068e4
serge <sergeyv39@gmail.com>
2022-01-27 10:12:41 +0200

    Initial commit

---
+++ mysite/news/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News, Category
+
+
+# Create your views here.
+def index(request):
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html',
+                  {'news': news, 'title': 'Список новостей', 'category': category})
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     return render(request, 'news/category.html', {'news': news, 'category': category})
