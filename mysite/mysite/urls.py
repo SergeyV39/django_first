c974147f9235dd7b1128e373ef45717de46e8c64
serge <sergeyv39@gmail.com>
2022-02-03 19:56:01 +0200

    add page about us

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
-
+    path('', include('about.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
